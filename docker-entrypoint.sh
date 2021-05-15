dd26f78fa615624e12a706a8aa25916ce0bea493
cdryzun <45552084+cdryzun@users.noreply.github.com>
2021-05-15 22:41:21 +0800

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -71,8 +71,8 @@ if [ ${LISTEN_PORT} != "443" ]; then
 	#Find TCP/UDP line numbers and use sed to replace the lines
 	TCPLINE=$(grep -rne 'tcp-port =' ocserv.conf | grep -Eo '^[^:]+')
 	UDPLINE=$(grep -rne 'udp-port =' ocserv.conf | grep -Eo '^[^:]+')
-	sed -i "$(TCPLINE)s/.*/tcp-port = ${LISTEN_PORT}/" /config/ocserv.conf
-	sed -i "$(UDPLINE)s/.*/tcp-port = ${LISTEN_PORT}/" /config/ocserv.conf
+	sed -i "${TCPLINE}s/.*/tcp-port = ${LISTEN_PORT}/" /config/ocserv.conf
+	sed -i "${UDPLINE}s/.*/tcp-port = ${LISTEN_PORT}/" /config/ocserv.conf
 fi
 
 if [[ ${TUNNEL_MODE} == "all" ]]; then
